e765ecdde7501eb7a2de1b96dd5ec96a902c9f75
Vincent Reslou <reslouv@yahoo.fr>
2024-07-09 17:57:30 +0200

    refactor: change the print to st.write

--- init_data.py
+++ init_data.py
@@ -1,18 +1,18 @@
 import pandas as pd
 import numpy as np
-
+import streamlit as st
 def load_csv(file_path,header=0,sep=','):
 
     return pd.read_csv(file_path,header=header,sep=sep)
 def display_description(data:pd.DataFrame):
     #display the first and the last row of the data
-    print(data.head())
-    print(data.tail())
+    st.write(data.head())
+    st.write(data.tail())
 def statistical_summary(data:pd.DataFrame): 
     """: Provide a basic statistical summary of the data, including the
     number of lines and columns, the name of the columns, the number of missing values per
     column, etc."""
-    print(data.describe())
-    print(data.info())
-    print(data.isnull().sum())
+    st.write(data.describe())
+    st.write(data.info())
+    st.write(data.isnull().sum())
 
