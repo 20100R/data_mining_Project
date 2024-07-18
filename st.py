ecc50e8b9f0e592bf6b2a1f22ed8b71fdcbf43a5
Vincent Reslou <reslouv@yahoo.fr>
2024-07-18 15:12:45 +0200

    st:ajoute la persistance des data entre les opérations visualization: améliore la visualition

--- st.py
+++ st.py
@@ -42,7 +42,6 @@ if uploaded_file is not None:
     
     # Load the data using the selected separator and header
     data = pd.read_csv(uploaded_file, header=header, sep=sep)
-        
     # Display Data
     st.write("Data first 5 value:")
     st.write(data.head())
@@ -57,68 +56,74 @@ if uploaded_file is not None:
     st.markdown('```plaintext\n' + s + '\n```')
     st.write("Missing Data:")
     st.write(data.isnull().sum())
+    if 'data' not in st.session_state:
+        st.session_state['data'] = data
 
+        st.write("Data loaded successfully")
+    else:
+        st.write("Data loaded successfully")
+if 'data' in st.session_state:
+    st.header("Data Pre-processing and Cleaning")
+    #add a button to delete the empty rows and columns
+    if st.button("Delete empty rows and columns"):
+        st.session_state['data'] = pre_processing.delete_empty_row_col(st.session_state['data'])
+        st.write(st.session_state['data'])
 
-st.header("Data Pre-processing and Cleaning")
-#add a button to delete the empty rows and columns
-if st.button("Delete empty rows and columns"):
-    data = pre_processing.delete_empty_row_col(data)
-    st.write(data)
+    #add a button to replace the missing values
+    method = st.selectbox("Choose the method to replace the missing values",['mean','median','mode'])
+    if st.button("Replace missing values"):
+        st.session_state['data'] = pre_processing.replace_missing_values(st.session_state['data'],method)
+        st.write(st.session_state['data'])
+    #add a selection box to choose the normalization method
+    normalization_method = st.selectbox("Choose the normalization method",['Min-Max','Z-standardization'])
+    #add a button to normalize the data
+    if st.button("Normalize the data"):
+        if normalization_method == 'Min-Max':
+            st.session_state['data'] = pre_processing.normalize_min_max(st.session_state['data'])
+        elif normalization_method == 'Z-standardization':
+            st.session_state['data'] = pre_processing.normalize_z_standardization(st.session_state['data'])
+        st.write(st.session_state['data'])
 
-#add a button to replace the missing values
-method = st.selectbox("Choose the method to replace the missing values",['mean','median','mode'])
-if st.button("Replace missing values"):
-    data = pre_processing.replace_missing_values(data,method)
-    st.write(data)
-#add a selection box to choose the normalization method
-normalization_method = st.selectbox("Choose the normalization method",['Min-Max','Z-standardization'])
-#add a button to normalize the data
-if st.button("Normalize the data"):
-    if normalization_method == 'Min-Max':
-        data = pre_processing.normalize_min_max(data)
-    elif normalization_method == 'Z-standardization':
-        data = pre_processing.normalize_z_standardization(data)
-    st.write(data)
+    #add a button to display the histograms
+    if st.button("Display histograms"):
+        st.write(st.session_state['data'])
+        visualization.histograms(st.session_state['data'])
+    #add a button to display the box plots
+    if st.button("Display box plots"):
+        st.write(st.session_state['data'])
+        visualization.box_plots(st.session_state['data'])
 
-#add a button to display the histograms
-if st.button("Display histograms"):
-    st.write(data)
-    visualization.histograms(data)
-#add a button to display the box plots
-if st.button("Display box plots"):
-    st.write(data)
-    visualization.box_plots(data)
+    #add a selection box to choose the clustering algorithm
+    algorithm = st.selectbox("Choose the clustering algorithm",['kmeans','dbscan'])
+    if algorithm == 'dbscan':
+        eps = st.number_input("Enter the maximum distance between two samples",min_value=1.0)
+        min_samples = st.number_input("Enter the number of samples in a neighborhood for a point to be considered as a core point",min_value=1)
+    elif algorithm == 'kmeans':
 
-#add a selection box to choose the clustering algorithm
-algorithm = st.selectbox("Choose the clustering algorithm",['kmeans','dbscan'])
-if algorithm == 'dbscan':
-    eps = st.number_input("Enter the maximum distance between two samples",min_value=1.0)
-    min_samples = st.number_input("Enter the number of samples in a neighborhood for a point to be considered as a core point",min_value=1)
-elif algorithm == 'kmeans':
+        #add a selection box to choose the number of clusters
+        cluster_number =st.selectbox("Choose the number of clusters",[1,2,3,4,5])
+    target_column = st.selectbox("Choose the x",st.session_state['data'].columns)
+    target_column2 = st.selectbox("Choose the y",st.session_state['data'].columns)
+    #add a button to perform clustering
+    if st.button("Perform clustering"):
+        if algorithm == 'kmeans':
+            st.session_state['data'], test_score = ml.perform_clustering(st.session_state['data'],algorithm,n_clusters=cluster_number)
+            #fait une figure pour afficher les clusters
+            visualization.plot_clusters(st.session_state['data'],target_column,target_column2)
+            st.session_state['data'] = st.session_state['data']
+            st.write(f"Test score: {test_score}")
+        elif algorithm == 'dbscan':
+            st.session_state['data'] = ml.perform_clustering(st.session_state['data'],algorithm,eps=eps,min_samples=min_samples)
+        st.write(st.session_state['data'])
 
-    #add a selection box to choose the number of clusters
-    cluster_number =st.selectbox("Choose the number of clusters",[1,2,3,4,5])
-target_column = st.selectbox("Choose the x",data.columns)
-target_column2 = st.selectbox("Choose the y",data.columns)
-#add a button to perform clustering
-if st.button("Perform clustering"):
-    if algorithm == 'kmeans':
-        data, test_score = ml.perform_clustering(data,algorithm,n_clusters=cluster_number)
-        #fait une figure pour afficher les clusters
-        visualization.plot_clusters(data,target_column,target_column2)
-        st.write(f"Test score: {test_score}")
-    elif algorithm == 'dbscan':
-         data = ml.perform_clustering(data,algorithm,eps=eps,min_samples=min_samples)
-    st.write(data)
-
-#add a selection box to choose the prediction algorithm
-algorithm = st.selectbox("Choose the prediction algorithm",['linear_regression','decision_tree'])
-#add a button to perform prediction
-target_column = st.selectbox("Choose the target column",data.columns)
-if st.button("Perform prediction"):
-    if algorithm == 'linear_regression':
-        data, scores = ml.perform_prediction(data,target_column,algorithm)
-    elif algorithm == 'decision_tree':
-        data, scores = ml.perform_prediction(data,target_column,algorithm)
-    st.write(data)
-    st.write(scores)
+    #add a selection box to choose the prediction algorithm
+    algorithm = st.selectbox("Choose the prediction algorithm",['linear_regression','decision_tree'])
+    #add a button to perform prediction
+    target_column = st.selectbox("Choose the target column",st.session_state['data'].columns)
+    if st.button("Perform prediction"):
+        if algorithm == 'linear_regression':
+            st.session_state['data'], scores = ml.perform_prediction(st.session_state['data'],target_column,algorithm)
+        elif algorithm == 'decision_tree':
+            st.session_state['data'], scores = ml.perform_prediction(st.session_state['data'],target_column,algorithm)
+        st.write(st.session_state['data'])
+        st.write(scores)
--- visualization.py
+++ visualization.py
@@ -33,9 +33,17 @@ def plot_clusters(data:pd.DataFrame,target_column:str,target_column2:str):
     Parameters:
     data (pd.DataFrame): The DataFrame containing the cluster labels.
     """
-    fig = plt.figure()
-    plt.scatter(data.iloc[:,0], data.iloc[:,1], c=data['Cluster'])
-    plt.xlabel(target_column)
-    plt.ylabel(target_column2)
-    plt.title("Clustered Data")
+
+    st.write(data["Cluster"])
+    
+    fig, ax = plt.subplots()
+    scatter = ax.scatter(data[target_column], data[target_column2], c=data["Cluster"], cmap='viridis')
+    
+    # Add a color bar
+    legend1 = ax.legend(*scatter.legend_elements(), title="Clusters")
+    ax.add_artist(legend1)
+    
+    ax.set_xlabel(target_column)
+    ax.set_ylabel(target_column2)
+    ax.set_title("Clustered Data")
     st.pyplot(fig)
